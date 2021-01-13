fb138b2d32b69d8aca216d90a35a2f01ac3b02d2
Juan Santana <juansantanajunior@gmail.com>
2021-01-12 21:07:05 -0500

    designing material ui layout

--- e-commerce/pages/_app.js
+++ e-commerce/pages/_app.js
@@ -2,8 +2,12 @@ import '../styles/globals.css'
 import { StoreProvider } from '../components/Store';
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 
 function MyApp({ Component, pageProps }) {
   return(

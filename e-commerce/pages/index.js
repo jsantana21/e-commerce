e9df2502b296bbbdc8f0a3c9f08b720e5ca977e7
Juan Santana <juansantanajunior@gmail.com>
2021-01-13 23:12:58 -0500

    finished styling product list home page

--- e-commerce/pages/index.js
+++ e-commerce/pages/index.js
@@ -1,26 +1,42 @@
-import Head from 'next/head'
 import Layout from '../components/Layout';
-import styles from '../styles/Home.module.css'
 import getCommerce from '../utils/commerce'
 import { Alert } from '@material-ui/lab';
+import { Grid, Box, Card, CardActionArea, CardContent, CardMedia, Slide, Typography } from '@material-ui/core';
+import Link  from 'next/link';
 
 export default function Home(props) {
   const { products } = props;
   return (
     <Layout title = "Home" commercePublicKey ={props.commercePublicKey}>
-      {
-        products.length === 0 && <Alert>No available products...</Alert>
-      }
-      <main className={styles.main}>
-
+      { products.length === 0 && <Alert>No available products...</Alert> }
+      <Grid container spacing = {1}>
       {products.map((product) => (
-        <div key ={product.id}>
-          <img src = {product.media.source} alt = {product.name}/>
-          <p>{product.name}</p>
-          <p>{product.price.formatted_with_symbol}</p>
-        </div>
+        <Grid item md={3}>
+          <Slide key={product.id} direction="up" in={true}>
+            <Grid item md={15}>
+              <Card>
+                <Link href={`/products/${product.permalink}`}>
+                  <CardActionArea>
+                    <CardMedia component="img" alt={product.name} image={product.media.source} />
+                    <CardContent>
+                      <Typography gutterBottom variant="body2" color="textPrimary" component="p">
+                        {product.name}
+                      </Typography>
+                      <Box>
+                        <Typography variant="body1" color="textPrimary" component="p">
+                          {product.price.formatted_with_symbol}
+                        </Typography>
+                      </Box>
+                    </CardContent>
+                  </CardActionArea>
+                </Link>
+              </Card>
+            </Grid>
+          </Slide>
+        </Grid>
+
         ))}
-      </main>
+      </Grid>
 
     </Layout>
   );

5b24b71cb95a6bcbcd6ba4e20b986d5da5ee76f9
Juan Santana <juansantanajunior@gmail.com>
2021-01-15 00:21:11 -0500

    module isssue fixed

--- e-commerce/components/Store.js
+++ e-commerce/components/Store.js
@@ -1,20 +1,9 @@
-import { FlareSharp } from '@material-ui/icons';
 import React, {createContext, useReducer} from 'react';
 
 export const Store = createContext();
 
 function reducer(state, action){
     switch (action.type){
-        case CART_RETRIEVE_REQUEST:
-            return {
-                ...state,
-                cart: {loading: true},
-            };
-        case CART_RETRIEVE_SUCCESS:
-            return {
-                ...state,
-                cart: {loading: false, data: action.payload}
-            };
     default:
         return state;
     }

25625920bc0465388dfd1a57c97ebd7cfc062806
Juan Santana <juansantanajunior@gmail.com>
2021-01-14 23:58:17 -0500

    beginning add to cart implementation

--- e-commerce/components/Store.js
+++ e-commerce/components/Store.js
@@ -1,9 +1,20 @@
+import { FlareSharp } from '@material-ui/icons';
 import React, {createContext, useReducer} from 'react';
 
 export const Store = createContext();
 
 function reducer(state, action){
     switch (action.type){
+        case CART_RETRIEVE_REQUEST:
+            return {
+                ...state,
+                cart: {loading: true},
+            };
+        case CART_RETRIEVE_SUCCESS:
+            return {
+                ...state,
+                cart: {loading: false, data: action.payload}
+            };
     default:
         return state;
     }

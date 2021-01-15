264677f1e402f5167411e4ee774a4b4bd383cc32
Juan Santana <juansantanajunior@gmail.com>
2021-01-15 00:13:04 -0500

    Revert "Update Store.js"
    This reverts commit 042a59f097617d78cf7fa1d7b33dfaa39bb63abb.

--- e-commerce/components/Store.js
+++ e-commerce/components/Store.js
@@ -1,9 +1,21 @@
 import { FlareSharp } from '@material-ui/icons';
 import React, {createContext, useReducer} from 'react';
+//import { CART_RETRIEVE_REQUEST, CART_RETRIEVE_SUCCESS } from '../utils/constants';
+
 export const Store = createContext();
 
 function reducer(state, action){
     switch (action.type){
+        /*case CART_RETRIEVE_REQUEST:
+            return {
+                ...state,
+                cart: {loading: true},
+            };
+        case CART_RETRIEVE_SUCCESS:
+            return {
+                ...state,
+                cart: {loading: false, data: action.payload}
+            };*/
     default:
         return state;
     }

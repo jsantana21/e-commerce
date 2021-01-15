b49adb102d185d00eeffb8e06ffadd9917c47825
Juan Santana <juansantanajunior@gmail.com>
2021-01-15 00:02:58 -0500

    Revert "issues fixed & finished product details page"
    This reverts commit dbf4ff9d77b7ecbc8aeea0ba8de54ca9c3b3dfcd.

--- e-commerce/pages/products/[id].js
+++ e-commerce/pages/products/[id].js
@@ -1,137 +1,63 @@
-import Layout from '../../components/Layout';
-import getCommerce from '../../utils/commerce'
+import Layout from '../components/Layout';
+import getCommerce from '../utils/commerce'
 import { Alert } from '@material-ui/lab';
-import { Grid, Button, Box, Card, List, ListItem, MenuItem, Select, Slide, Typography } from '@material-ui/core';
+import { Grid, Box, Card, CardActionArea, CardContent, CardMedia, Slide, Typography } from '@material-ui/core';
 import Link  from 'next/link';
-import { useState } from 'react';
-import { useStyles } from '../../utils/styles';
+import { ListItem } from '@material-ui/core';
 
 export default function Product(props) {
   const { product } = props;
-
-  const [quantity, setQuantity] = useState(1);
-
-  const classes = useStyles();
-  
-  const addToCartHandler = async() => {
-      console.log('Finish Add to Cart function')
-  }
   return (
     <Layout title = "Home" commercePublicKey ={props.commercePublicKey}>
-        <Slide direction = "up" in={true}>
-            <Grid container spacing={1}>
-                <Grid item md={6}>
-                    <img 
-                    src = {product.media.source}
-                    alt = {product.name}
-                    className = {classes.largeImage}
-                    />
-                </Grid>
-                <Grid item md={3} xs={12}>
-                    <List>
-                        <ListItem>
-                            <Typography
-                            gutterBottom
-                            variant = "h6"
-                            color = "textPrimary"
-                            component = "h1"
-                        >
-                            {product.name}
-                            </Typography>
-                        </ListItem>
-                        <ListItem>
-                            <Box
-                            dangerouslySetInnerHTML = {{ __html: product.description }}
-                            ></Box>
-                        </ListItem>
-                    </List>
-                </Grid>
-                <Grid item md={3} xs={12}>
-                    <Card>
-                        <List>
-                            <ListItem>
-                                <Grid container>
-                                    <Grid item xs={6}>
-                                        Price
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        {product.price.formatted_with_symbol}
-                                    </Grid>
-                                </Grid>
-                            </ListItem>
-                            <ListItem>
-                                <Grid alignItems="center" container>
-                                    <Grid item xs={6}>
-                                        Status
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        {product.quantity > 0 ? (
-                                            <Alert icon={false} severity="success">
-                                                In Stock
-                                            </Alert>
-                                        ) : (
-                                            <Alert icon={false} severity="error">
-                                                Out of Stock
-                                            </Alert>
-                                        )}
-                                    </Grid>
-                                </Grid>
-                            </ListItem>
-                            {product.quantity > 0 && (
-                                <>
-                                <ListItem>
-                                    <Grid container justify="flex-end">
-                                        <Grid item xs={6}>
-                                            Quantity
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <Select
-                                            labelId="quanitity-label"
-                                            id="quanitity"
-                                            fullWidth
-                                            onChange={(e) => setQuantity(e.target.value)}
-                                            value = {quantity}
-                                            >
-                                                {[...Array(product.quantity).keys()].map((x) => (
-                                                    <MenuItem key = {x+1} value = {x+1}>
-                                                        {x+1}
-                                                    </MenuItem>
-                                                ))}
-                                            </Select>
-                                        </Grid>
-                                    </Grid>
-                                </ListItem>
-                                <ListItem>
-                                    <Button
-                                    type = "button"
-                                    fullWidth
-                                    variant="contained"
-                                    color = "primary"
-                                    onClick={addToCartHandler}
-                                    >
-                                        Add to Cart
-                                    </Button>
-                                </ListItem>
-                                </>
-                            )}
-                        </List>
-                    </Card>
-                </Grid>
+        <Slide direction = "up" in = {true}>
+        <Grid container spacing ={1}>
+            <Grid item md = {6}>
+                <img src = {product.media.source} alt = {product.name} className = {classes.largeImage}/>
             </Grid>
+            <Grid item md={3} xs={12}>
+            <List>
+              <ListItem>
+                <Typography gutterBottom variant="h6" color="textPrimary" component="h1">
+                  {product.name}
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Box dangerouslySetInnerHTML={{ __html: product.description }}> </Box>
+              </ListItem>
+            </List>
+          </Grid>
+          <Gird item md = {3} xs ={12}> 
+          <Card>
+              <List>
+                  <ListItem>
+                      <Grid container>
+                          <Grid item xs={6}> Price </Grid>
+                          <Grid item xs={6}> {product.price.formatted_with_symbol} </Grid>
+                      </Grid>
+                  </ListItem>
+                  <ListItem>
+                  <Grid alignItems="center" container>
+                    <Grid item xs={6}> Status </Grid>
+                    <Grid item xs={6}> {product.quantity > 0 ? (<Alert icon={false} severity="success"> In Stock </Alert> ) 
+                    : ( <Alert icon={false} severity="error"> Out of Stock </Alert> )}
+                    </Grid>
+                  </Grid>
+                </ListItem>
+                
+              </List>
+          </Card>
+          </Gird>
+        </Grid>
+
         </Slide>
+
     </Layout>
   );
 }
 
-export async function getServerSideProps( { params } ) {
-    const { id } = params;
-    const commerce = getCommerce();
-    const product = await commerce.products.retrieve(id, {
-        type: 'permalink',
-    });
-    return {
-        props: {
-            product,
-        },
-    };
+export async function getStaticProps({params}) {
+  const { id } = params;
+  const commerce = getCommerce();
+  const product = await commerce.products.retrieve(id, { type: 'permalink', });
+  return { props: { product, } , };
 }

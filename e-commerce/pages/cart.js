e55c7d8681dbd22c973cc7f481001f7f924cc66c
Juan Santana <juansantanajunior@gmail.com>
2021-01-15 22:50:49 -0500

    progress

--- e-commerce/pages/cart.js
+++ e-commerce/pages/cart.js
@@ -7,6 +7,8 @@ import Link  from 'next/link';
 import { useStyles } from '../utils/styles';
 import { useContext } from 'react';
 import { Store } from '../components/Store';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+
 
 export default function Cart(props) {
   const classes = useStyles();
@@ -14,7 +16,40 @@ export default function Cart(props) {
   const { products } = props;
   return (
     <Layout title = "Cart" commercePublicKey ={props.commercePublicKey}>
-
+        {cart.loading ? (
+            <CircularProgress/>
+        ): cart.data.line_items.length === 0 ? (
+            <Alert icon={false} severity="error">
+                Empty Cart <Link href="/"> Go shop to fill up the Cart</Link>
+            </Alert>
+        ):(
+            <React.Fragment>
+                <Typography variant="h1" component="h1">
+                    Shopping Cart 
+                </Typography>
+                <Slide direction="up" in={true}>
+                    <Grid container spacing={1}>
+                        <Grid item md={9}>
+                            <TableContainer>
+                                <Table aria-label="Orders">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>Name</TableCell>
+                                            <TableCell align="right">Quantity</TableCell>
+                                            <TableCell align="right">Price</TableCell>
+                                            <TableCell>Action</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Grid>
+                    </Grid>
+                </Slide>
+            </React.Fragment>
+        )}
     </Layout>
   );
 }
